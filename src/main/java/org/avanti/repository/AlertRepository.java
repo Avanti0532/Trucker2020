eb28e6974dd322d925bce420f3e893de24717f5f
Avanti0532 <42561372+Avanti0532@users.noreply.github.com>
2020-06-01 11:56:49 -0500

    fix(module-api):fix the issue of fetching alerts and location

--- src/main/java/org/avanti/repository/AlertRepository.java
+++ src/main/java/org/avanti/repository/AlertRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface AlertRepository extends CrudRepository<Alert, String> {
-    @Query(value = "SELECT * FROM Alert WHERE priority='HIGH' and HOUR(TIMEDIFF(CURRENT_TIMESTAMP,alert_time_stamp)) <= 2",
+    @Query(value = "SELECT * FROM Alert WHERE priority='HIGH' and (TIMESTAMPDIFF(HOUR,alert_time_stamp,CURRENT_TIMESTAMP)) <= 2",
     nativeQuery = true)
     List<Alert> findAllHigh();
 

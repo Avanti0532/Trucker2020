8b336f0d6bc31b572655f743d8327bd4597a2d62
Avanti0532 <42561372+Avanti0532@users.noreply.github.com>
2020-06-01 13:25:00 -0500

    feat(module-api):add swagger-ui for api documentation

--- src/main/java/org/avanti/controller/AlertController.java
+++ src/main/java/org/avanti/controller/AlertController.java
@@ -1,5 +1,8 @@
 package org.avanti.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.avanti.entity.Alert;
 import org.avanti.service.AlertService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +20,22 @@ public class AlertController {
     private AlertService service;
 
     @RequestMapping(method= RequestMethod.GET,value="fetchbyhighpriority")
+    @ApiOperation(value = "fetch all high alerts within 2 hours")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     public List<Alert> findAllHigh(){
         return service.findAllHigh();
     }
 
     @RequestMapping(method = RequestMethod.GET, value="{id}")
+    @ApiOperation(value = "Fetch all alerts of a vehicle by using vin")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     public List<Alert> findByVin(@PathVariable("id") String vin){
         return service.findByVin(vin);
     }
--- src/main/java/org/avanti/controller/ReadingController.java
+++ src/main/java/org/avanti/controller/ReadingController.java
@@ -1,5 +1,8 @@
 package org.avanti.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.avanti.entity.Reading;
 import org.avanti.service.ReadingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,11 @@ public class ReadingController {
 
     @CrossOrigin(origins = "*", maxAge = 4800)
     @RequestMapping(method= RequestMethod.POST)
+    @ApiOperation(value = "Create readings")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     public void create(@RequestBody Reading reading){
         service.create(reading);
     }

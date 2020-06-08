1b247a5e0701d820f37b6e3c104fd7a142d26626
Avanti0532 <42561372+Avanti0532@users.noreply.github.com>
2020-06-07 20:18:20 -0500

    test(module-api): add test for location service class

--- src/main/java/org/avanti/service/LocationServiceImpl.java
+++ src/main/java/org/avanti/service/LocationServiceImpl.java
@@ -3,11 +3,13 @@ package org.avanti.service;
 import org.avanti.entity.Location;
 import org.avanti.repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Primary
 public class LocationServiceImpl implements LocationService {
 
     @Autowired

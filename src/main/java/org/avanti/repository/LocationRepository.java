502be378b3c23cbdf2b8ce208138dec9de7db594
Avanti0532 <42561372+Avanti0532@users.noreply.github.com>
2020-06-15 22:02:50 -0500

    Update location repository

--- src/main/java/org/avanti/repository/LocationRepository.java
+++ src/main/java/org/avanti/repository/LocationRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface LocationRepository extends CrudRepository<Location, String> {
-    @Query(value = "SELECT * FROM Location WHERE vin = :id  and TIMESTAMPDIFF(MINUTE ,time_stamp,CURRENT_TIMESTAMP) <= 30",
+    @Query(value = "SELECT * FROM location WHERE vin = :id  and TIMESTAMPDIFF(MINUTE ,time_stamp,CURRENT_TIMESTAMP) <= 30",
             nativeQuery = true)
     List<Location> findVinTime(@Param("id") String vinId);
 }

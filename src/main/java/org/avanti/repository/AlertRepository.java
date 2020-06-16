178e7cbe252bfdf3a8a683906ab3f3e2a393c12c
Avanti0532 <42561372+Avanti0532@users.noreply.github.com>
2020-06-15 22:02:15 -0500

    Update alert repository

--- src/main/java/org/avanti/repository/AlertRepository.java
+++ src/main/java/org/avanti/repository/AlertRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface AlertRepository extends CrudRepository<Alert, String> {
-    @Query(value = "SELECT * FROM Alert WHERE priority='HIGH' and (TIMESTAMPDIFF(HOUR,alert_time_stamp,CURRENT_TIMESTAMP)) <= 2 ORDER BY alert_time_stamp ASC",
+    @Query(value = "SELECT * FROM alert WHERE priority='HIGH' and (TIMESTAMPDIFF(HOUR,alert_time_stamp,CURRENT_TIMESTAMP)) <= 2 ORDER BY alert_time_stamp ASC",
     nativeQuery = true)
     List<Alert> findAllHigh();
     List<Alert> findByVin(String vin);
